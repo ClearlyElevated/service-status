a8763a74e1f9bffcc2a1a72d1d3b072672a9143d
ClearlyElevated <51843882+ClearlyElevated@users.noreply.github.com>
2019-10-19 09:31:43 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 var monitors = [
-    "m783364201-65a81aa6774153dd114659dc",  //Place your first Key Here
-    "ur749395-9dd145171d38225293e765e2", //Place your next key here! <- Keep duplicating this line for more than 2 keys!
+    "m782868659-c5618ee1cb30b226f8fe93d9",  //Place your first Key Here
+    "", //Place your next key here! <- Keep duplicating this line for more than 2 keys!
 
     
 ];
 var title = 'ClearlyElevated Services Page'; //Page <title> and heading at top of page
-var refreshtime = 60; //How often to refresh in seconds
+var refreshtime = ; //How often to refresh in seconds
 var footermessage = ''; //HTML to go in footer
